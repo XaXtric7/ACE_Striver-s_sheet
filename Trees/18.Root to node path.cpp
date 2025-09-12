c60b9191f332d5d745462e28edecde592ce0d17d
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-09-12 19:27:07 +0530

    root to node path

---
+++ Trees/18.Root to node path.cpp
@@ -0,0 +1,78 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode
+{
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+
+class Solution
+{
+public:
+    bool getPath(TreeNode *root, vector<int> &arr, int x)
+    {
+        if (!root)
+            return false;
+
+        arr.push_back(root->val);
+
+        if (root->val == x)
+            return true;
+
+        if (getPath(root->left, arr, x) || getPath(root->right, arr, x))
+            return true;
+
+        arr.pop_back();
+        return false;
+    }
+
+public:
+    vector<int> solve(TreeNode *A, int B)
+    {
+        vector<int> arr;
+        if (A == NULL)
+        {
+            return arr;
+        }
+        getPath(A, arr, B);
+        return arr;
+    }
+};
