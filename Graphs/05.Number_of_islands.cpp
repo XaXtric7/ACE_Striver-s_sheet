ad5b50ea596209582fda9d2e518066e5cbcec77b
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-10-19 19:20:56 +0530

    Number of Islands

---
+++ Graphs/05.Number_of_islands.cpp
@@ -0,0 +1,60 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    void bfs(int row, int col, vector<vector<int>> &visited, vector<vector<char>> &grid)
+    {
+        visited[row][col] = 1;
+        queue<pair<int, int>> q;
+        q.push({row, col});
+        int n = grid.size();
+        int m = grid[0].size();
+
+        while (!q.empty())
+        {
+            int row = q.front().first;
+            int col = q.front().second;
+            q.pop();
+
+            // traverse in the neighbour and mark them if its a island
+            for (int delrow = -1; delrow <= 1; delrow++)
+            {
+                for (int delcol = -1; delcol <= 1; delcol++)
+                {
+                    int nrow = row + delrow;
+                    int ncol = col + delcol;
+                    if (nrow >= 0 && nrow < n && ncol >= 0 && ncol < m && grid[row][col] == '1' && !visited[nrow][ncol])
+                    {
+                        visited[nrow][ncol] = 1;
+                        q.push({nrow, ncol});
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    int numIslands(vector<vector<char>> &grid)
+    {
+        int n = grid.size();    // number of rows
+        int m = grid[0].size(); // number of cols
+
+        vector<vector<int>> visited(n, vector<int>(m, 0));
+        int count = 0;
+
+        for (int row = 0; row < n; row++)
+        {
+            for (int col = 0; col < m; col++)
+            {
+                if (!visited[row][col] && grid[row][col] == '1')
+                {
+                    count++;
+                    bfs(row, col, visited, grid);
+                }
+            }
+        }
+        return count;
+    }
+};
