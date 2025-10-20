591baf29a32e91f485c534fbf63cdae54413ec45
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-10-20 13:36:00 +0530

    Detect a cycle in und_graph(DFS)

---
+++ Graphs/09.Detect_a_cycle_using_DFS.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+private:
+    bool dfs(int node, int parent, vector<int> &visited, vector<int> adj[])
+    {
+        visited[node] = 1;
+
+        for (auto adjacentNode : adj[node])
+        {
+            if (!visited[adjacentNode])
+            {
+                if (dfs(adjacentNode, node, visited, adj))
+                    return true;
+            }
+            else if (adjacentNode != parent)
+            {
+                // A visited node not equal to parent -> cycle detected
+                return true;
+            }
+        }
+        return false;
+    }
+
+public:
+    bool isCycle(int V, vector<int> adj[])
+    {
+        vector<int> visited(V, 0);
+
+        for (int i = 0; i < V; i++)
+        {
+            if (!visited[i])
+            {
+                if (dfs(i, -1, visited, adj))
+                    return true;
+            }
+        }
+        return false;
+    }
+};
