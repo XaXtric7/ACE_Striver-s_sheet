fe65889f3cfb04ba5ba03cb3cfbbe8b40db24dd4
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-12-23 16:49:28 +0530

    Bellman Ford Algorithm

---
+++ Graphs/36.Bellman_Ford_Algorithm.cpp
@@ -0,0 +1,41 @@
+#include <bits/stdc++.h>
+using namespace std;
+const int INF = 1e8;
+
+class Solution
+{
+public:
+    vector<int> bellman_ford(int V, vector<vector<int>> &edges, int S)
+    {
+        vector<int> dist(V, INF);
+        dist[S] = 0;
+        for (int i = 0; i < V; i++)
+        {
+            for (auto it : edges)
+            {
+                int u = it[0];
+                int v = it[1];
+                int wt = it[2];
+
+                if (dist[u] != INF && dist[u] + wt < dist[v])
+                {
+                    dist[v] = dist[u] + wt;
+                }
+            }
+        }
+        // N_th relaxation to check negative cycle
+
+        for (auto it : edges)
+        {
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+            if (dist[u] != INF && dist[u] + wt < dist[v])
+            {
+                return {-1};
+            }
+        }
+
+        return dist;
+    }
+};
