22135bf042dc3035914101fc3d488bd95174b2e7
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2025-12-30 12:40:28 +0530

    Disjoint sets

--- Graphs/40.Disjoint_Set.cpp
+++ Graphs/40.Disjoint_Set.cpp
@@ -2,16 +2,18 @@
 using namespace std;
 class DisjointSet
 {
-    vector<int> rank, parent;
+    vector<int> rank, parent, size;
 
 public:
     DisjointSet(int n)
     {
         rank.resize(n + 1, 0);
         parent.resize(n + 1);
+        size.resize(n + 1);
         for (int i = 0; i <= n; i++)
         {
             parent[i] = i;
+            size[i] = 1;
         }
     }
 
@@ -45,6 +47,25 @@ public:
             rank[ulp_u]++;
         }
     }
+
+    void unionBySize(int u, int v)
+    {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+
+        if (ulp_u == ulp_v)
+            return;
+        if (size[ulp_u] < size[ulp_v])
+        {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else
+        {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
 };
 
 // Harcoding example
@@ -52,6 +73,7 @@ public:
 int main()
 {
     DisjointSet ds(7);
+    // choose either unionByRank or unionBySize
     ds.unionByRank(1, 2);
     ds.unionByRank(2, 3);
     ds.unionByRank(3, 4);
