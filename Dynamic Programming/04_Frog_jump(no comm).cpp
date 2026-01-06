6f4b6c063078a4269a8af274f0c26aba00299232
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-01-06 13:29:41 +0530

    Frog Jump - Recursion, Memoization, Tabulation, Space opti.(NO COMMENTS)

---
+++ Dynamic Programming/04_Frog_jump(no comm).cpp
@@ -0,0 +1,129 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Greedy will fail, [Recursion method] (gives TLE for large values)
+// TC = O(2^n), SC = O(n)
+class RecursionSolution
+{
+public:
+    int f(int ind, vector<int> &heights)
+    {
+        if (ind == 0)
+            return 0;
+
+        int left = f(ind - 1, heights) + abs(heights[ind] - heights[ind - 1]);
+        int right = INT_MAX;
+        if (ind > 1)
+        {
+            right = f(ind - 2, heights) + abs(heights[ind] - heights[ind - 2]);
+        }
+        return min(left, right);
+    }
+
+    int solve(int n, vector<int> &heights)
+    {
+        return f(n - 1, heights);
+    }
+};
+
+// Memoization for optimization
+// TC = O(n), SC = O(n)  (dp + recursion stack)
+class MemoizationSolution
+{
+public:
+    int f(int ind, vector<int> &heights, vector<int> &dp)
+    {
+        if (ind == 0)
+            return 0;
+        if (dp[ind] != -1)
+            return dp[ind];
+
+        int left = f(ind - 1, heights, dp) + abs(heights[ind] - heights[ind - 1]);
+        int right = INT_MAX;
+        if (ind > 1)
+        {
+            right = f(ind - 2, heights, dp) + abs(heights[ind] - heights[ind - 2]);
+        }
+        return dp[ind] = min(left, right);
+    }
+
+    int solve(int n, vector<int> &heights)
+    {
+        vector<int> dp(n, -1); // dp-array
+        return f(n - 1, heights, dp);
+    }
+};
+
+// Recursion --> Tabulation
+// TC = O(n), SC = O(n)
+class TabulationSolution
+{
+public:
+    int solve(int n, vector<int> &heights)
+    {
+        vector<int> dp(n, -1); // dp-array
+        dp[0] = 0;
+
+        for (int i = 1; i < n; i++)
+        {
+            int fs = dp[i - 1] + abs(heights[i] - heights[i - 1]);
+            int ss = INT_MAX;
+            if (i > 1)
+            {
+                ss = dp[i - 2] + abs(heights[i] - heights[i - 2]);
+            }
+            dp[i] = min(fs, ss);
+        }
+        return dp[n - 1];
+    }
+};
+
+// Tabulation --> Space optimization
+// TC = O(n), SC = O(1)
+class SpaceOptimizedSolution
+{
+public:
+    int solve(int n, vector<int> &heights)
+    {
+        int prev2 = 0; // dp[i-2]
+        int prev1 = 0; // dp[i-1]
+
+        for (int i = 1; i < n; i++)
+        {
+            int fs = prev1 + abs(heights[i] - heights[i - 1]);
+            int ss = INT_MAX;
+            if (i > 1)
+                ss = prev2 + abs(heights[i] - heights[i - 2]);
+
+            int cur = min(fs, ss);
+            prev2 = prev1;
+            prev1 = cur;
+        }
+        return prev1;
+    }
+};
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        vector<int> heights(n);
+        for (int i = 0; i < n; i++)
+        {
+            cin >> heights[i];
+        }
+
+        // Choose ONE solution to run
+        SpaceOptimizedSolution solver;
+        // RecursionSolution solver;        // will TLE
+        // MemoizationSolution solver;
+        // TabulationSolution solver;
+
+        cout << solver.solve(n, heights) << endl;
+    }
+    return 0;
+}
