2fac835680b46a0067f95cb923ab86a178b424ef
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-01-25 15:00:28 +0530

    3D dp Cherry Pickup

--- Dynamic Programming/14_Cherry_Pickup_II.cpp
+++ Dynamic Programming/14_Cherry_Pickup_II.cpp
@@ -90,3 +90,106 @@ int maximumChocolates(int r, int c, vector<vector<int>> &grid)
     return f(0, 0, c - 1, r, c, grid, dp);
 }
 */
+
+/*
+// Using Tabulation..
+
+int maximumChocolates(int n, int m, vector<vector<int>> &grid)
+{
+    // dp[r][c][c]
+    vector<vector<vector<int>>> dp(n, vector<vector<int>>(m, vector<int>(m, 0)));
+
+    // base case
+    for (int j1 = 0; j1 < m; j1++)
+    {
+        for (int j2 = 0; j2 < m; j2++)
+        {
+            if (j1 == j2)
+                dp[n - 1][j1][j2] = grid[n - 1][j1];
+            else
+                dp[n - 1][j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+        }
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (int j1 = 0; j1 < m; j1++)
+        {
+            for (int j2 = 0; j2 < m; j2++)
+            {
+                int maxi = -1e8;
+                // exploration of the path in 3 directions
+                for (int dj1 = -1; dj1 <= 1; dj1++)
+                {
+                    for (int dj2 = -1; dj2 <= 1; dj2++)
+                    {
+                        int value = 0;
+                        if (j1 == j2)
+                            value = grid[i][j1];
+                        else
+                            value = grid[i][j1] + grid[i][j2];
+                        if (j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m)
+                            value += dp[i + 1][j1 + dj1][j2 + dj2];
+                        maxi = max(maxi, value);
+                    }
+                }
+                dp[i][j1][j2] = maxi;
+            }
+        }
+    }
+    return dp[0][0][m - 1];
+}
+*/
+
+// Using Space Optimization..
+
+int maximumChocolates(int n, int m, vector<vector<int>> &grid)
+{
+    // 1DP -> two variables
+    // 2DP -> 1D DP
+    // 3DP -> 2D DP
+
+    vector<vector<int>> front(m, vector<int>(m, 0));
+    vector<vector<int>> curr(m, vector<int>(m, 0));
+
+    // base case
+    for (int j1 = 0; j1 < m; j1++)
+    {
+        for (int j2 = 0; j2 < m; j2++)
+        {
+            if (j1 == j2)
+                front[j1][j2] = grid[n - 1][j1];
+            else
+                front[j1][j2] = grid[n - 1][j1] + grid[n - 1][j2];
+        }
+    }
+
+    for (int i = n - 2; i >= 0; i--)
+    {
+        for (int j1 = 0; j1 < m; j1++)
+        {
+            for (int j2 = 0; j2 < m; j2++)
+            {
+                int maxi = -1e8;
+                // exploration of the path in 3 directions
+                for (int dj1 = -1; dj1 <= 1; dj1++)
+                {
+                    for (int dj2 = -1; dj2 <= 1; dj2++)
+                    {
+                        int value = 0;
+                        if (j1 == j2)
+                            value = grid[i][j1];
+                        else
+                            value = grid[i][j1] + grid[i][j2];
+                        if (j1 + dj1 >= 0 && j1 + dj1 < m && j2 + dj2 >= 0 && j2 + dj2 < m)
+                            value += front[j1 + dj1][j2 + dj2];
+                        maxi = max(maxi, value);
+                    }
+                }
+                curr[j1][j2] = maxi;
+            }
+        }
+        front = curr;
+    }
+    return front[0][m - 1];
+}
