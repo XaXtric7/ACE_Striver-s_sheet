c1a090d2d925b31a95de90a1a62ac8c1f0a49a9a
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-01-31 09:24:23 +0530

    Minimum Absolute Sum Difference

--- Dynamic Programming/17_Minimum_Absolute_Sum_Difference.cpp
+++ Dynamic Programming/17_Minimum_Absolute_Sum_Difference.cpp
@@ -8,35 +8,55 @@ int minSubsetSumDifference(vector<int> &arr, int n)
         totSum += arr[i];
 
     int k = totSum;
-    vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
 
-    // Base case: sum 0 is always possible
-    for (int i = 0; i < n; i++)
-        dp[i][0] = true;
+    // vector<vector<bool>> dp(n, vector<bool>(k + 1, false));
 
-    if (arr[0] <= k)
-        dp[0][arr[0]] = true;
+    // // Base case: sum 0 is always possible
+    // for (int i = 0; i < n; i++)
+    //     dp[i][0] = true;
+
+    // if (arr[0] <= k)
+    //     dp[0][arr[0]] = true;
+
+    // for (int ind = 1; ind < n; ind++)
+    // {
+    //     for (int target = 1; target <= k; target++)
+    //     {
+    //         bool notTake = dp[ind - 1][target];
+    //         bool take = false;
+    //         if (arr[ind] <= target)
+    //             take = dp[ind - 1][target - arr[ind]];
+
+    //         dp[ind][target] = take || notTake;
+    //     }
+    // }
+
+    // Space optimized
+    vector<bool> prev(k + 1, 0), curr(k + 1, 0);
+    prev[0] = curr[0] = true;
+    prev[arr[0]] = true;
 
     for (int ind = 1; ind < n; ind++)
     {
         for (int target = 1; target <= k; target++)
         {
-            bool notTake = dp[ind - 1][target];
+            bool notTake = prev[target];
             bool take = false;
             if (arr[ind] <= target)
-                take = dp[ind - 1][target - arr[ind]];
-
-            dp[ind][target] = take || notTake;
+                take = prev[target - arr[ind]];
+            curr[target] = take || notTake;
         }
-
-        int mini = 1e9;
-        for (int s1 = 0; s1 <= totSum / 2; s1++)
-        {
-            if (dp[n - 1][s1])
-            {
-                int s2 = totSum - s1;
-                mini = min(mini, abs(s2 - s1));
-            }
-        }
-        return mini;
+        prev = curr;
     }
+
+    int mini = 1e9;
+    for (int s1 = 0; s1 <= totSum / 2; s1++)
+    {
+        if (prev[s1])
+        {
+            int s2 = totSum - s1;
+            mini = min(mini, abs(s2 - s1));
+        }
+    }
+    return mini;
+}
