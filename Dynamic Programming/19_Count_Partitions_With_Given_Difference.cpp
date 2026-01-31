0525e7b3c81073bf986f59aa400851bc104d37df
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-01 01:36:19 +0530

    Count partitions with given difference

---
+++ Dynamic Programming/19_Count_Partitions_With_Given_Difference.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int mod = (int)(1e9 + 7);
+int f(int ind, int sum, vector<int> &num, vector<vector<int>> &dp)
+{
+    if (ind == 0)
+    {
+        if (sum == 0 && num[0] == 0)
+            return 2;
+        if (sum == 0 || sum == num[0])
+            return 1;
+        return 0;
+    }
+    if (dp[ind][sum] != -1)
+        return dp[ind][sum];
+    int notTake = f(ind - 1, sum, num, dp);
+    int take = 0;
+    if (num[ind] <= sum)
+        take = f(ind - 1, sum - num[ind], num, dp);
+    return dp[ind][sum] = (take + notTake) % mod;
+}
+
+int findWays(vector<int> &num, int target)
+{
+    int n = num.size();
+    vector<vector<int>> dp(n, vector<int>(target + 1, -1));
+    return f(n - 1, target, num, dp);
+}
+
+int countPartitions(int n, int d, vector<int> &arr)
+{
+    int totSum = 0;
+    for (auto it : arr)
+        totSum += it;
+    if (totSum - d < 0 || ((totSum - d) % 2))
+        return false;
+    return findWays(arr, (totSum - d) / 2);
+}
