acb43795ae6f808d9c1764c58032baeafaabfdf2
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-01 10:23:53 +0530

    Count partitions with given difference

--- Dynamic Programming/19_Count_Partitions_With_Given_Difference.cpp
+++ Dynamic Programming/19_Count_Partitions_With_Given_Difference.cpp
@@ -37,3 +37,58 @@ int countPartitions(int n, int d, vector<int> &arr)
         return false;
     return findWays(arr, (totSum - d) / 2);
 }
+
+// Using Tabulation
+
+int findWaysTab(vector<int> &num, int target)
+{
+    int n = num.size();
+    vector<vector<int>> dp(n, vector<int>(target + 1, 0));
+    if (num[0] == 0)
+        dp[0][0] = 2;
+    else
+        dp[0][0] = 1;
+    if (num[0] != 0 && num[0] <= target)
+        dp[0][num[0]] = 1;
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int sum = 0; sum <= target; sum++)
+        {
+            int notTake = dp[ind - 1][sum];
+            int take = 0;
+            if (num[ind] <= sum)
+                take = dp[ind - 1][sum - num[ind]];
+
+            dp[ind][sum] = (take + notTake) % mod;
+        }
+    }
+    return dp[n - 1][target];
+}
+
+// Using Space Optimization...
+
+int findWaysTab(vector<int> &num, int target)
+{
+    int n = num.size();
+    vector<int> prev(target + 1, 0), curr(target + 1, 0);
+    if (num[0] == 0)
+        prev[0] = 2;
+    else
+        prev[0] = 1;
+    if (num[0] != 0 && num[0] <= target)
+        prev[num[0]] = 1;
+    for (int ind = 1; ind < n; ind++)
+    {
+        for (int sum = 0; sum <= target; sum++)
+        {
+            int notTake = prev[sum];
+            int take = 0;
+            if (num[ind] <= sum)
+                take = prev[sum - num[ind]];
+
+            curr[sum] = (take + notTake) % mod;
+        }
+        prev = curr;
+    }
+    return prev[target];
+}
