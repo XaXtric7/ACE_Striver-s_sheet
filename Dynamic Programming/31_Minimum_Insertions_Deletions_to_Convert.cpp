25093897ef9ec444ab74df06fef23f0c3b514bea
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-14 04:25:53 +0530

    Minimum Insertions/Deletions to Convert String A to String B

--- Dynamic Programming/31_Minimum_Insertions_Deletions_to_Convert.cpp
+++ Dynamic Programming/31_Minimum_Insertions_Deletions_to_Convert.cpp
@@ -19,7 +19,8 @@ int lcsTab(string s, string t)
         {
             if (s[i - 1] == t[j - 1])
                 dp[i][j] = 1 + dp[i - 1][j - 1];
-            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+            else
+                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
         }
     }
     return dp[n][m];
