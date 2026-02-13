8e22f9a5db9223fa5d8f68516982d198e3e5902c
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-14 04:25:33 +0530

    Minimum Insertions/Deletions to Convert String A to String B

---
+++ Dynamic Programming/31_Minimum_Insertions_Deletions_to_Convert.cpp
@@ -0,0 +1,55 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// Using Tabulation...
+
+int lcsTab(string s, string t)
+{
+    int n = s.size();
+    int m = t.size();
+    vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
+
+    for (int j = 0; j <= m; j++)
+        dp[0][j] = 0;
+    for (int i = 0; i <= n; i++)
+        dp[i][0] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+                dp[i][j] = 1 + dp[i - 1][j - 1];
+            dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+        }
+    }
+    return dp[n][m];
+}
+
+// Using Space Optimization...
+
+int lcsSpaceOpti(string s, string t)
+{
+    int n = s.size();
+    int m = t.size();
+    vector<int> prev(m + 1, 0), curr(m + 1, 0);
+
+    for (int j = 0; j <= m; j++)
+        prev[j] = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            if (s[i - 1] == t[j - 1])
+                curr[j] = 1 + prev[j - 1];
+            else
+                curr[j] = max(prev[j], curr[j - 1]);
+        }
+        prev = curr;
+    }
+    return prev[m];
+}
+
+int canYouMake(string &str, string &ptr)
+{
+    return str.size() + ptr.size() - (2 * lcsTab(str, ptr));
+}
