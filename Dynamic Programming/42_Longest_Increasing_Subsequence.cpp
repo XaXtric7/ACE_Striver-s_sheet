6f5e15e9a2e0bdc07015479fca504faed5c5fa61
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-17 17:31:34 +0530

    Longest Increasing Subsequence

--- Dynamic Programming/42_Longest_Increasing_Subsequence.cpp
+++ Dynamic Programming/42_Longest_Increasing_Subsequence.cpp
@@ -83,9 +83,29 @@ int longestIncreasingSubsequence_SpaceOpti(int arr[], int n)
     return next[-1 + 1];
 }
 
+// Using Algorithm...
+
+int longestIncreasingSubsequence1(int arr[], int n)
+{
+    vector<int> dp(n, 1);
+    int maxi = 1;
+    for (int i = 0; i < n; i++)
+    {
+        for (int prev = 0; prev < i; prev++)
+        {
+            if (arr[prev] < arr[i])
+            {
+                dp[i] = max(dp[i], 1 + dp[prev]);
+            }
+        }
+        maxi = max(maxi, dp[i]);
+    }
+    return maxi;
+}
+
 // Using Optimal LIS (Binary Search – O[n log n])
 
-int longestIncreasingSubsequence(int arr[], int n)
+int longestIncreasingSubsequence2(int arr[], int n)
 {
     vector<int> temp;
 
