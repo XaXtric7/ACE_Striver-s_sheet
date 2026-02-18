cee26be66633dd20f5b2a1982e4f1f04b0ddc494
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-18 18:53:56 +0530

    Longest String Chain

--- Dynamic Programming/44_Longest_String_Chain.cpp
+++ Dynamic Programming/44_Longest_String_Chain.cpp
@@ -35,3 +35,61 @@ int longestStrChain(vector<string> &words)
 
     return maxi;
 }
+
+// Strive's sol...
+
+bool checkPossible(string &s1, string &s2)
+{
+    // s1 should be exactly one character bigger than s2
+    if (s1.size() != s2.size() + 1)
+        return false;
+
+    int first = 0;  // pointer for s1
+    int second = 0; // pointer for s2
+
+    while (first < s1.size())
+    {
+        if (second < s2.size() && s1[first] == s2[second])
+        {
+            first++;
+            second++;
+        }
+        else
+        {
+            first++; // skip one char in s1
+        }
+    }
+
+    return (second == s2.size());
+}
+
+int longestStrChain2(vector<string> &arr)
+{
+
+    int n = arr.size();
+
+    // Sort by length
+    sort(arr.begin(), arr.end(), [](string &a, string &b)
+         { return a.size() < b.size(); });
+
+    vector<int> dp(n, 1);
+    int maxi = 1;
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int prev = 0; prev < i; prev++)
+        {
+
+            if (checkPossible(arr[i], arr[prev]) &&
+                dp[prev] + 1 > dp[i])
+            {
+
+                dp[i] = dp[prev] + 1;
+            }
+        }
+
+        maxi = max(maxi, dp[i]);
+    }
+
+    return maxi;
+}
