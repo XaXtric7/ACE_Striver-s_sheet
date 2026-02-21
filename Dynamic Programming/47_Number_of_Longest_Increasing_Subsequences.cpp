e9f27b7ab946d9c6b3b1c09f46bfa8a5b3e121a5
XaXtric7 <sarthakdharmik10227xix@gmail.com>
2026-02-21 10:38:58 +0530

    Number of Longest Increasing Subsequences

--- Dynamic Programming/47_Number_of_Longest_Increasing_Subsequences.cpp
+++ Dynamic Programming/47_Number_of_Longest_Increasing_Subsequences.cpp
@@ -1,6 +1,76 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// Mine...
+
+int longestIncreasingSubsequence1(vector<int> &arr, int n)
+{
+    vector<int> dp1(n, 1);
+
+    // LIS from left
+    for (int i = 0; i < n; i++)
+    {
+        for (int prev = 0; prev < i; prev++)
+        {
+            if (arr[prev] < arr[i])
+            {
+                dp1[i] = max(dp1[i], 1 + dp1[prev]);
+            }
+        }
+    }
+
+    vector<int> dp2(n, 1);
+
+    // LIS from right (i.e., Longest Decreasing Subsequence)
+    for (int i = n - 1; i >= 0; i--)
+    {
+        for (int prev = n - 1; prev > i; prev--)
+        {
+            if (arr[prev] < arr[i])
+            {
+                dp2[i] = max(dp2[i], 1 + dp2[prev]);
+            }
+        }
+    }
+
+    int maxii = 0;
+
+    // Find maximum bitonic length
+    for (int i = 0; i < n; i++)
+    {
+        maxii = max(maxii, dp1[i] + dp2[i] - 1);
+    }
+
+    int count = 0;
+
+    // Count how many indices give maximum bitonic length
+    for (int i = 0; i < n; i++)
+    {
+        if (dp1[i] + dp2[i] - 1 == maxii)
+            count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+
+    vector<int> arr(n);
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+
+    cout << longestIncreasingSubsequence1(arr, n);
+
+    return 0;
+}
+
+// Striver's...
+
 int findNumberOfLIS(vector<int> &arr)
 {
     int n = arr.size();
